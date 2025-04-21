f0191e4d6ad9a16695efccd6a173c4c0a2246b7c
palyamrohith660 <palyamrohith660@gmail.com>
2025-04-21 22:12:03 +0530

    Delete 1.py

--- 1.py
+++
@@ -1,8 +0,0 @@
-import ctypes
-
-# Load SecuGen Fingerprint Library
-sgfplib = ctypes.WinDLL("C:\\Program Files\\SecuGen\\SDK\\Inc\\SGFPLib.dll")
-
-# Initialize library
-sgfplib.Create()
-print("SecuGen SDK initialized successfully!")
